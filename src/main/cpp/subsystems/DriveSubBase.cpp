1061e33048018bc4040e97cd988cf5ba1f8b52f0
1828 Boxerbots <35279026+1828Boxerbots@users.noreply.github.com>
2022-08-06 09:52:22 -0700

    DriveSubBase - Briscoe McCune
    The MoveTank and Move Arcade are set up in DriveSubBase.

---
+++ src/main/cpp/subsystems/DriveSubBase.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "subsystems/DriveSubBase.h"
+
+DriveSubBase::DriveSubBase() = default;
+
+// This method will be called once per scheduler run
+    void DriveSubBase::Periodic() 
+{
+
+}
+
+    void DriveSubBase::MoveTank(double left, double right)
+{
+   LeftMotor(left);
+   RightMotor(right);
+}
+
+    void DriveSubBase::MoveArcade(double x, double y)
+{
+ double left = y + x;
+ double right = y - x;
+
+ MoveTank(left, right);
+}
---
+++ src/main/include/subsystems/DriveSubBase.h
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+
+class DriveSubBase : public frc2::SubsystemBase 
+{
+ public:
+  DriveSubBase();
+
+  void Periodic() override;
+
+  virtual void Init();
+
+  virtual void LeftMotor(double speed);
+
+  virtual void RightMotor(double speed);
+
+  void MoveTank(double left, double right);
+
+  void MoveArcade(double x, double y);
+
+ private:
+
+
+};
